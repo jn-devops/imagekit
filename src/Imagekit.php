43b8ed393d88c801003a2107f13bbd4cba48f0b4
Gari Vivar <ggvivar@joy-nostalg.com>
2024-10-09 15:01:54 +0800

    added function for array input

--- src/Imagekit.php
+++ src/Imagekit.php
@@ -44,4 +44,41 @@ class Imagekit
             ], 500);
         }
     }
+    public function uploadFilev2(array $jsonInput)
+    {
+        $imageUrl = $jsonInput['imageUrl'];
+        $folderPath = isset($jsonInput['folderPath']) ? $jsonInput['folderPath'] : config('imagekit')['apiKey'];
+        $authKey = base64_encode(config('imagekit')['apiKey'] . ':' . $imageUrl);
+
+        $formData = [
+            [
+                'name' => 'file',
+                'contents' => fopen($imageUrl, 'r'),
+            ],
+            [
+                'name' => 'fileName',
+                'contents' => $jsonInput['fileName'].'.png',
+            ],
+            [
+                'name' => 'folder',
+                'contents' => $folderPath,
+            ],
+        ];
+
+        try {
+            $client = new Client;
+            $response = $client->post('https://upload.imagekit.io/api/v1/files/upload', [
+                'headers' => [
+                    'Authorization' => 'Basic '.$authKey,
+                ],
+                'multipart' => $formData,
+            ]);
+
+            return $response->getBody();
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Error uploading file: '.$e->getMessage(),
+            ], 500);
+        }
+    }
 }
